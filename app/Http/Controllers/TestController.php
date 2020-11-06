07984cfda8d337aa7108c90a772b941ead434d4e
lixuetao <1651928858@qq.com>
2020-11-06 13:55:56 +0800

    微信调用token

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -37,11 +37,29 @@ class TestController extends Controller
 
 //   获取access_token
     public function getAccessToken(){
-        $grant_type="client_credential";
-        $appid="wxc8e73af28fb246ce";
-        $secret="e3b11750e1de175e6f94cde4ebdfed72";
-        $url="https://api.weixin.qq.com/cgi-bin/token?grant_type=".$grant_type."&appid=".$appid."&secret=".$secret;
-        $url=file_get_contents($url);
-        return $url;
+        $key="1234";
+        $response=Redis::get($key);
+        if(!$response){
+            echo "没有缓存";
+            $grant_type="client_credential";
+            $appid="wxc8e73af28fb246ce";
+            $secret="e3b11750e1de175e6f94cde4ebdfed72";
+            $url="https://api.weixin.qq.com/cgi-bin/token?grant_type=".$grant_type."&appid=".$appid."&secret=".$secret;
+
+            $arrContextOptions = [
+                'ssl' => [
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ]
+            ];
+            $response = file_get_contents($url, false, stream_context_create($arrContextOptions));
+
+            $response=json_decode($response,true);
+            $response=$response['access_token'];
+            Redis::set($key,$response);
+            Redis::expire($key,3600);
+        }
+        echo $response;
     }
+
 }
